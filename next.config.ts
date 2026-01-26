8830fe102a0abcdb8ab789a9d671d11d115d08f6
tmz1dka <mahamud12sep@gmail.com>
2026-01-26 20:16:36 +0200

    chore: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  // Write build output to ./build instead of the locked .next folder
-  distDir: "build",
 };
 
 export default nextConfig;
